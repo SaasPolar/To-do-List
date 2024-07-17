2453e77aa961391bcef028a5572a5a67d1357536
SaasPolar <saydotrabalho@gmail.com>
2024-07-17 10:28:56 -0300

    To Do List

---
+++ script.js
@@ -0,0 +1,45 @@
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoList = document.getElementById('todo-list');
+
+todoForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const newTask = todoInput.value;
+
+  if (newTask === '') {
+      alert('Adicione uma tarefa, por favor');
+      return;
+  }
+  todoInput.value = '';
+  addTask(newTask);
+});
+
+function addTask(task) {
+  const listItem = document.createElement('li');
+  const taskText = document.createElement('span');
+  taskText.textContent = task;
+  listItem.appendChild(taskText);
+
+  const checkBox = document.createElement('input');
+  checkBox.setAttribute('type', 'checkbox');
+  listItem.appendChild(checkBox);
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  listItem.appendChild(deleteButton);
+
+  todoList.appendChild(listItem);
+
+  checkBox.addEventListener('change', function() {
+      if (this.checked) {
+          taskText.style.textDecoration = 'line-through';
+      } else {
+          taskText.style.textDecoration = 'none';
+      }
+  });
+ 
+  deleteButton.addEventListener('click', function() {
+      todoList.removeChild(listItem);
+  });
+
+}
